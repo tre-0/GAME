8cc29ea5ee356a2cb6674d77b6e937ebe5f2ebf0
tre-0 <tarrenle231@gmail.com>
2026-01-30 11:39:29 -0600

    Project settings and assets

---
+++ Assets/NewMonoBehaviourScript.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+/*
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+
+    int number = 5;
+    short smallerNumber = 2;
+    double otherNum = 1.23;
+    decimal decNum = 123.293;
+
+    [SerializeField]
+        float num1 = 0;
+    [SerializeField]
+        float num2 = 0;
+   
+    [SerializeField] bool flag = false;
+    [SerializeField] string f;
+    char singleChar = 'a';
+    string test = "55";
+
+    [SerializeField, Range(0, 10)] int maximum;
+    [SerializeField, Range(0, 10)] int minimum;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log(5 - 2);
+        Debug.Log(5 + 2);
+        Debug.Log (decNum / 2);
+
+        Debug.Log (num1 / num2);
+        Debug.Log (num1 % num2);
+        Debug.Log(num1 + num2);
+        Debug.Log(MathF .Pow(25));
+        Debug.Log(5 + int.parse(singleChar));
+        int num = int.parse(test);
+        Debug.Log(5 + test);
+        Debug.Log(Random.range(1, 10));
+
+        int myVar = 1;
+            {
+                Debug.Log(myVar);
+
+                char myChar = 'a';
+            }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+*/
